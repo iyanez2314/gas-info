1155ff29c201fbdd7c1ac3158ec83cddf7fb60c4
Isaac Yanez <isaac231467@gmail.com>
2023-07-15 14:51:39 -0500

    changed the nav bar with the name of the site and the li tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center py-4 space-x-2 px-10">
       <div className="flex text-white text-3xl font-bold items-center space-x-2">
-        <Link href="/">Logo</Link>
+        <Link href="/">Gas Info</Link>
       </div>
       <nav className="text-white">
         <ul className="flex items-center space-x-4">
-          <li>FAQ</li>
+          <li>Made by Isaac Yanez</li>
         </ul>
       </nav>
     </header>
