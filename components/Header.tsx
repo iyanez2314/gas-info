8e6c14bb643699cdecdcbfd6f4ce4035d9f81d79
Isaac Yanez <isaac231467@gmail.com>
2023-07-15 15:02:17 -0500

    changed the site name

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center py-4 space-x-2 px-10">
       <div className="flex text-white text-3xl font-bold items-center space-x-2">
-        <Link href="/">Gas Info</Link>
+        <Link href="/">Gas Geek</Link>
       </div>
       <nav className="text-white">
         <ul className="flex items-center space-x-4">
